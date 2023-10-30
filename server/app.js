7a3f9b5d5be001027dc4892a14c5cb634e760326
Ainoa Urquidi <148345452+ainoaur-uma@users.noreply.github.com>
2023-10-30 22:40:33 +0100

    separando index.js en app.js y server.js

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const dotenv = require('dotenv').config();
 // Parse application/json
 app.use(bodyParser.json());
 
+// Importa las rutas de las entidades
 const usuariosRoutes = require('../api/routes/usuariosRoutes');
 const rolesRoutes = require('../api/routes/rolesRoutes');
 const agendasRoutes = require('../api/routes/agendasRoutes');
@@ -17,6 +18,7 @@ const pacientesRoutes = require('../api/routes/pacientesRoutes');
 const hceRoutes = require('../api/routes/hceRoutes');
 const episodiosRoutes = require('../api/routes/episodiosRoutes');
 
+// Define las rutas para las entidades
 app.use('/usuarios', usuariosRoutes);
 app.use('/roles', rolesRoutes);
 app.use('/agendas', agendasRoutes);
@@ -27,15 +29,9 @@ app.use('/pacientes', pacientesRoutes);
 app.use('/hce', hceRoutes);
 app.use('/episodios', episodiosRoutes);
 
-// Obtiene el puerto desde las variables de entorno o utiliza un valor predeterminado
-const port = process.env.PORT || 3000;
-
-// Inicia el servidor en el puerto especificado
-app.listen(port, () => {
-  console.log(`Servidor en ejecución en el puerto ${port}`);
-});
-
 // Ruta raíz para la página de inicio
 app.get('/', (req, res) => {
   res.send('¡Bienvenido a la aplicación de la clínica!');
 });
+
+module.exports = app;
--- server/db_connection.js
+++ server/db_connection.js
@@ -18,13 +18,4 @@ dbConnection.connect((err) => {
   }
 });
 
-// dbConnection.query('SELECT * from rol', function (error, results, fields) {
-//   if (error) {
-//     throw error;
-//   }
-//   results.forEach((result) => {
-//     console.log(result);
-//   });
-// });
-
 module.exports = dbConnection;
